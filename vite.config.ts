b86f3a4464025656d2a355bfec32746bff06a706
Loilo <florian.reuschel@gmx.de>
2024-01-09 10:22:22 +0100

    fix: add icons to demo

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vitest/config'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { VitePWA } from 'vite-plugin-pwa'
 
+const base = '/node-js-php-data/'
+
 export default defineConfig({
   root: './demo',
-  base: '/node-js-php-data/',
+  base,
   resolve: {
     alias: {
       '@fontsource/jetbrains-mono': '@fontsource/jetbrains-mono'
@@ -14,7 +16,42 @@ export default defineConfig({
     nodePolyfills({
       include: ['path']
     }),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'JS to PHP data',
+        short_name: 'JS to PHP data',
+        description: 'Convert JavaScript data structures to PHP.',
+        lang: 'en',
+        display: 'fullscreen',
+        background_color: '#1c202b',
+        theme_color: '#1c202b',
+        icons: [
+          ...[120, 152, 167, 180, 1024].map(size => ({
+            src: `${base}icons/ios_${size}x${size}.png`,
+            sizes: `${size}x${size}`,
+            types: 'image/png'
+          })),
+          {
+            src: `${base}icons/ios_1024x1024.png`,
+            sizes: '1024x1024',
+            type: 'image/png'
+          },
+          ...[48, 72, 96, 144, 168, 192, 512].map(size => ({
+            src: `${base}icons/android_${size}x${size}.png`,
+            sizes: `${size}x${size}`,
+            type: 'image/png',
+            purpose: 'any'
+          })),
+          {
+            src: `${base}icons/maskable.png`,
+            sizes: '1024x1024',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
   test: {
     globals: true,
